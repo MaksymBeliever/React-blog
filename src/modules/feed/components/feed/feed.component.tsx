f31effa50ce60b73b3665e4a2316d370563c2359
max <max.beliba@alternatestates.com>
2023-10-17 16:57:14 +0300

    Added Feed Toggle button to layout

--- src/modules/feed/components/feed/feed.component.tsx
+++ src/modules/feed/components/feed/feed.component.tsx
@@ -1,6 +1,7 @@
 import { FC } from 'react';
 import { Container } from '../../../../common/components/container/container.component';
 import { ArticleList } from '../article-list/article-list.component';
+import { FeedToggleComponent } from '../feed-toggle/feed-toggle.component';
 
 interface FeedProps {
 
@@ -9,6 +10,7 @@ interface FeedProps {
 export const Feed: FC<FeedProps> = () => {
     return (
         <Container>
+            <FeedToggleComponent />
             <div className="flex">
                 <ArticleList />
                 <div className="w-1/4 columns-2xs">tags</div>
